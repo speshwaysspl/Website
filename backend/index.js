0c953186edb77413853d975bc6d61546cf866a9e
speshwaysspl <speshwaysspl@gmail.com>
2025-12-05 14:00:06 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,8 @@ const corsOptions = {
   origin: [
     'http://localhost:8080',
     'http://localhost:8081',
+    'https://www.speshway.com',
+    'https://speshway.com',
     'http://localhost:3000',
     'http://127.0.0.1:8080',
     'http://127.0.0.1:8081',
